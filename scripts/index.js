766e08b2f06b95dc3a35d8810c9545c2a45ec43a
Daniel Cunha <danielcunha@MacBook-Pro-de-Daniel.local>
2022-02-19 13:17:02 -0800

    Menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,11 @@ const routes = [new Route("#main", "pages/main.html", `scripts/mainPage.js`)];
 
 Router.init("root", routes);
 
+mobileMenu.addEventListener("click", () => {
+  let menu = document.getElementById("menu");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
